c34e22a77746b2032c55d73adf05628990f26a0f
David Gershuni <vidgersh@gmail.com>
2017-06-16 13:29:15 -0700

    Adds router, begins implementation of firebase

---
+++ config.js
@@ -0,0 +1,14 @@
+var fs = require('fs')
+
+const setups = {
+  dev: {
+    apiKey: "AIzaSyC9bsf70eTpA7v6j8zf1ZxrRg1g7A1oZb8",
+    authDomain: "boxspring-81172.firebaseapp.com",
+    databaseURL: "https://boxspring-81172.firebaseio.com",
+    projectId: "boxspring-81172",
+    storageBucket: "boxspring-81172.appspot.com",
+    messagingSenderId: "421029558857"
+  }
+}
+
+fs.writeFileSync('lib/client/firebaseConfig.js', 'module.exports = ' + JSON.stringify(setups["dev"]))
