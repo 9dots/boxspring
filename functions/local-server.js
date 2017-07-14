e907d965e6acf151dbc6a1a3d205f2bf31953b60
David Gershuni <vidgersh@gmail.com>
2017-07-13 22:07:25 -0700

    Server caches bundles if unchanged

--- functions/local-server.js
+++ functions/local-server.js
@@ -28,17 +28,14 @@ function main(req, res) {
 	const projectId = req.query.projectId;
 	console.log("Got projectId ", projectId)
 	writeProjectFileTreeToDisk(projectId)
-		.then((val) => {
-			console.log("About to create bundle...", val)
-			createAndSendBundle(projectId, req, res)
-		})
-		.catch((err) => {console.log("Error writing project tree to disk: ", err)})
+		.then(() => createAndSendBundle(projectId, req, res))
+		.catch((err) => console.log("Error writing project tree to disk: ", err))
 }
 
 function writeProjectFileTreeToDisk(projectId) {
 	console.log("Writing file tree to disk...")
 	return bucket.getFiles({
-		prefix: projectId,
+		prefix: projectId + "/",
 	})
 		.then((response) => {
 			let files = response[0]
@@ -73,49 +70,52 @@ function createAndSendBundle(projectId, req, res) {
 	console.log("Creating bundle...")
 	let bundle = ''
 	console.log("Installing modules...")		
-	execFile('npm', ['install'], {cwd: projectId}, function (err, stdin, stdout) {
-		if (err) { throw err }
-
-		console.log("Bundling with browserify...")
-		let b = spawn('browserify', ['index.js'], {cwd:projectId})
-
-		b.stdout.on('data', (data) => {
-			bundle += data
-		})
-
-		b.on('close', (exitCode) => {
-			if (exitCode == 0)
-				sendBundleToClient(bundle, projectId, req, res)
-			else 
-				bundleError(exitCode, res)
-		})
-	});
+	execFile('npm', ['install'], {cwd: projectId},
+		function browserifyAndSend(err, stdin, stdout) {
+			if (err) { throw err }
+			console.log("Bundling with browserify...")
+			
+			let b = spawn('browserify', ['index.js'], {cwd:projectId})
+			b.stdout.on('data', (data) => { bundle += data })
+			b.on('close', (exitCode) => {
+				if (exitCode == 0)
+					sendBundleToClient(bundle, projectId, req, res)
+				else 
+					bundleError(exitCode, res)
+			})
+		}
+	)
 }
-// TODO: Check for build errors...
+
+
 
 function bundleError(exitCode, res) {
 	res.write("Error: Browserify exited with ", exitCode)
 	res.status(503)
 	res.end()
 }
-// returnBundleToClient('this is a bundle....', 'p123')
 
 function sendBundleToClient(bundle, projectId, req, res) {
-	// console.log(h64)
 	let bundleHash = h64(bundle, 0xABCD).toString(36)
-	console.log(bundleHash)
+	console.log("bundle hash = ", bundleHash)
 
 	// After build, upload to cloud storage
-	let file = bucket.file(`${projectId}-bundles/${bundleHash}`)
-	let stream = file.createWriteStream({
-		metadata: { contentType: 'text/javascript'},
-  })
-		.on('finish', function() {
-			console.log(file)
-			console.log("Upload complete")
-			console.log("Download here: ", file.metadata.mediaLink)
+	let file = bucket.file(`bundles/${projectId}/${bundleHash}`)
+
+	file.exists().then((data) => {
+		let exists = data[0]
+		if (exists) {
 			res.end(file.metadata.mediaLink)
-		})
-	stream.write(bundle)
-	stream.end()
+		} else {
+			let stream = file.createWriteStream({
+				metadata: { contentType: 'text/javascript'},
+		  })
+				.on('finish', function() {
+					console.log("Bundle download url: ", file.metadata.mediaLink)
+					res.end(file.metadata.mediaLink)
+				})
+			stream.write(bundle)
+			stream.end()
+		}
+	})
 }
